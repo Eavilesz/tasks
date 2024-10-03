1187561294f6d54f3b5d7141b489a901a931bab0
Eavilesz <ernesto-av@hotmail.com>
2024-10-03 17:35:16 -0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import './App.css';
+import { TaskGrid } from './components/TaskGrid';
+
+function App() {
+  return <TaskGrid />;
+}
+
+export default App;
---
+++ src/components/TaskGrid.tsx
@@ -0,0 +1,5 @@
+import {} from 'react';
+
+export const TaskGrid = () => {
+  return <h1>Hello world</h1>;
+};
